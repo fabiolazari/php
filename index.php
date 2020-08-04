4fd48f4eccde379edfc596465c9f45a98c68bfb2
fabiolazari <62772477+fabiolazari@users.noreply.github.com>
2020-08-04 20:47:06 -0300

    Adicionei outro echo!

--- index.php
+++ index.php
@@ -1,7 +1,10 @@
 <?php
 
-	echo "Meu primeiro codigo php, Olá Mundo!";
+	echo "Meu primeiro codigo php, Olá Mundo 123 !";
 	echo "Bom dia!!!";
 	echo "Como você está?";
 	echo "Mais uma mensgem";
+
+	echo "Olá HCode!"
+
 ?>
